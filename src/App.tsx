ab0f09164acdc074565f3343a15a72fa282e2cf3
tomasbasso <tomas.basso@hotmail.com>
2026-06-03 12:45:21 -0300

    feat: App.tsx con Lenis+GSAP sync, ensamble completo de secciones

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,70 @@
-export default function App() { return null }
+import { useEffect } from 'react'
+import Lenis from 'lenis'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+import ScrollProgress from './components/ScrollProgress'
+import CustomCursor from './components/CustomCursor'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Stack from './components/Stack'
+import Projects from './components/Projects'
+import Experience from './components/Experience'
+import Education from './components/Education'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+gsap.registerPlugin(ScrollTrigger)
+
+export default function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    })
+
+    lenis.on('scroll', ScrollTrigger.update)
+
+    const tick = (time: number) => lenis.raf(time * 1000)
+    gsap.ticker.add(tick)
+    gsap.ticker.lagSmoothing(0)
+
+    ScrollTrigger.create({
+      trigger: '#hero',
+      start: 'top top',
+      end: 'bottom top',
+      scrub: true,
+      onUpdate: (self) => {
+        const p = self.progress
+        gsap.set('.hero-glow-top', { y: p * -80 })
+        gsap.set('.hero-glow-bottom', { y: p * 60 })
+      },
+    })
+
+    return () => {
+      lenis.destroy()
+      ScrollTrigger.killAll()
+      gsap.ticker.remove(tick)
+    }
+  }, [])
+
+  return (
+    <>
+      <ScrollProgress />
+      <CustomCursor />
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Stack />
+        <Projects />
+        <Experience />
+        <Education />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  )
+}
